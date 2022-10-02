ba3a20d81e5c52d8743d803cbc86ed6141018971
alumno <dam.sonia.fernandez@gmail.com>
2022-10-02 19:17:47 +0200

    creacion: nameBox

---
+++ lib/src/custom_views/nameBox.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class nameBox extends StatelessWidget{
+  final String Bxnombre;
+
+  const nameBox({Key? key, required this.Bxnombre}):super (key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.lightBlueAccent),
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Text(Bxnombre),
+        ),
+    );
+  }
+
+}
