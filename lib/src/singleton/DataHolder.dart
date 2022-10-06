e69066b578c5f1ba42ffb6507a3a326947915d00
Nia <103027252+SFP95@users.noreply.github.com>
2022-10-06 09:18:50 +0200

    modis dataholder

--- lib/src/singleton/DataHolder.dart
+++ lib/src/singleton/DataHolder.dart
@@ -2,15 +2,16 @@
 import 'package:my_first_flutter/src/fb_proyects/Perfil.dart';
 
 class DataHolder {
+  /*mensajes que podemos llevar a todas las pantallas que queremos arrastrar
+  por tas las pestallas en las que lo llamemos
+
+  Existe una forma de mata la ejecuión pero aun no lo ha explicado
+  */
   static final DataHolder _dataHolder=new DataHolder._internal();
+
 
   String sMensaje=" ";
   Perfil perfil=Perfil();
-
-  /*static DataHolder get instance{
-
-    return _dataHolder;
-  }*/
 
   DataHolder._internal(){
     sMensaje="BIENVENIDO ";
