fcfed7d4849ad5c1fbabd784efc03a31b0274b8a
alumno <dam.sonia.fernandez@gmail.com>
2022-10-13 19:30:42 +0200

    ajustes enn DataHolder y FbAdmin

--- lib/src/firebase/FbAdmin.dart
+++ lib/src/firebase/FbAdmin.dart
@@ -5,15 +5,18 @@ class FbAdmin {
   FirebaseFirestore db = FirebaseFirestore.instance;
 
   FbAdmin(){
-    Future<Perfil?> desgarPerfil(String? idPerfil) async{
-      final docRef = db.collection("perfiles").doc(idPerfil).withConverter(
-        fromFirestore: Perfil.fromFirestore,
-        toFirestore: (Perfil perfil, _) => perfil.toFirestore(),
-      );
-      final docSnap = await docRef.get();
-      return docSnap.data();
+
     }
+
+  Future<Perfil?> descargarPerfil(String? idPerfil) async{
+    final docRef = db.collection("perfiles").doc(idPerfil).withConverter(
+      fromFirestore: Perfil.fromFirestore,
+      toFirestore: (Perfil perfil, _) => perfil.toFirestore(),
+    );
+    final docSnap = await docRef.get();
+    return docSnap.data();
+
   }
-}
+
 
 
--- lib/src/singleton/DataHolder.dart
+++ lib/src/singleton/DataHolder.dart
@@ -1,5 +1,9 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:my_first_flutter/src/fb_proyects/Perfil.dart';
+
+import '../fb_proyects/Room.dart';
+import '../firebase/FbAdmin.dart';
 
 class DataHolder {
   /*mensajes que podemos llevar a todas las pantallas que queremos arrastrar
@@ -9,16 +13,39 @@ class DataHolder {
   */
   static final DataHolder _dataHolder=new DataHolder._internal();
 
+  String sCOLLECTION_ROOMS_NAME="rooms";
+  String sCOLLECTION_TEXTS_NAME="texts";
 
   String sMensaje=" ";
   Perfil perfil=Perfil();
+  Room selectedChatRoom = Room();
+  FbAdmin fbAdmin = FbAdmin();
 
   DataHolder._internal(){
     sMensaje="BIENVENIDO ";
   }
 
   factory DataHolder(){
-
     return _dataHolder;
+  }
+
+  /**
+   * @param: none
+   * @return Funcion que espera la descarga del perfil y lo guardo en la clase DataHolder
+   */
+  Future<void> descargarMIPerfil() async{
+    perfil=await fbAdmin.descargarPerfil(FirebaseAuth.instance.currentUser?.uid) as Perfil;
+  }
+
+  /**
+   * @param idPerfil: Id del perfil que quiero descargar.
+   * @return Funcion que espera la descarga del perfil y lo guardo en la clase DataHolder
+   */
+  Future<void> descargarPerfilGenerico(String? idPerfil) async{
+    await fbAdmin.descargarPerfil(idPerfil) as Perfil;
+  }
+
+  bool isMIPerfilDownloaded(){
+    return perfil!=null;
   }
 }
