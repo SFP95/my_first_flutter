37537a0602debde1ce04583583548bc62aa174e2
alumno <dam.sonia.fernandez@gmail.com>
2022-10-13 18:36:36 +0200

    construcción de FbAdmin

--- lib/src/firebase/FbAdmin.dart
+++ lib/src/firebase/FbAdmin.dart
@@ -1,10 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:my_first_flutter/src/fb_proyects/Perfil.dart';
 
-FbAdmin (){
+class FbAdmin {
+  FirebaseFirestore db = FirebaseFirestore.instance;
 
+  FbAdmin(){
+    Future<Perfil?> desgarPerfil(String? idPerfil) async{
+      final docRef = db.collection("perfiles").doc(idPerfil).withConverter(
+        fromFirestore: Perfil.fromFirestore,
+        toFirestore: (Perfil perfil, _) => perfil.toFirestore(),
+      );
+      final docSnap = await docRef.get();
+      return docSnap.data();
+    }
+  }
 }
 
 
-/*void descargarPerfil() async{
- await Perfil=FbAdmin.descargarPerfil() as Perfil;
-}*/
