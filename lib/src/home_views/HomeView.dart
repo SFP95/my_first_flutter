4362161082937e6a05ab12e14d80932835582c35
alumno <dam.sonia.fernandez@gmail.com>
2022-10-16 12:28:38 +0200

    listview version final (pero me salen errores)

--- lib/src/home_views/HomeView.dart
+++ lib/src/home_views/HomeView.dart
@@ -3,8 +3,10 @@ import 'dart:html';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_first_flutter/src/list_items/RoomItem.dart';
 import 'package:my_first_flutter/src/singleton/DataHolder.dart';
 import '../fb_proyects/Perfil.dart';
+import '../fb_proyects/Room.dart';
 
 class HomeView extends StatefulWidget{
 
@@ -21,6 +23,7 @@ class _HomeView extends State<HomeView> {
 
   String name = " **** ";
   bool blIsRefresBtnVisible = true;
+  List<QueryDocumentSnapshot<Room>> chatRooms= [];
 
   @override
   void initState() {
@@ -47,24 +50,35 @@ class _HomeView extends State<HomeView> {
     }
   }
 
-  final List<String> entries = <String>['A', 'B', 'C'];
-  final List<int> colorCodes = <int>[600, 500, 100];
+   void actulizarLista()async{
+     final docRef = db.collection("rooms").withConverter(fromFirestore: Room.fromFirestore,
+       toFirestore: (Room room, _) => room.toFirestore());
 
+     final docSnap= await docRef.get();
+
+     setState(() {
+       for(int i=0;i<docSnap.docs.length;i++){
+         chatRooms.add(docSnap.docs[i].data());
+       }
+     });
+
+
+
+   }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Colors.purple[50],
       body: Center(
           child:ListView.separated( //me vale para mi proyecto
               padding: const EdgeInsets.all(8),
-              itemCount: entries.length,
+              itemCount: chatRooms.length,
               itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 50,
-                  color: Colors.amber[colorCodes[index]],
-                  child: Center(child: Text('Entry ${entries[index]}')),
-                );
+                return RoomItem(
+                  Titulo: chatRooms[index].name!,
+                  );
               },
               separatorBuilder: (BuildContext context, int index) {
                 //aqui puedes poner cosas dentros d ela divissiones
