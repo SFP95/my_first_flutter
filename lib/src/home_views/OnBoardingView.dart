4d41b223c1bab53ece8e4e5e68b0c9e321507b4f
Nia <103027252+SFP95@users.noreply.github.com>
2022-11-28 13:28:25 +0100

    28.11.22

--- lib/src/home_views/OnBoardingView.dart
+++ lib/src/home_views/OnBoardingView.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:my_first_flutter/src/custom_views/RFInputText.dart';
-
 import '../fb_proyects/Perfil.dart';
 import '../singleton/DataHolder.dart';
 
